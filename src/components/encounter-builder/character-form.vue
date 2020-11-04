61143611a1bb77a033e485de938f7ccefda9341f
Dillon Rice <drice89@gmail.com>
2020-11-03 22:22:02 -0800

    add table

--- src/components/encounter-builder/character-form.vue
+++ src/components/encounter-builder/character-form.vue
@@ -5,15 +5,21 @@
             <input :name="char-name" type="text" v-model="name"/>
             <label for="char-level">Character Level</label>
             <input type="text" :name="char-level" v-model="level" />
+            <label for="init">HP</label>
+            <input type="text" :name="hp" v-model="hp" />
             <label for="init">Initiative Value</label>
             <input type="text" v-model="init" :name="init" />
             <input type="submit">
         </form>
-        <v-list>
-            <li v-for="(character, index) in characters" :key="index">
+        <v-data-table
+            :headers = "headers"
+            :items = "characters"
+            item-key = "name"
+        >
+            <!-- <li v-for="(character, index) in characters" :key="index">
                 {{ character.name }}
-            </li>
-        </v-list>
+            </li> -->
+        </v-data-table>
     </div>
 </template>
 
@@ -23,8 +29,15 @@
             return {
                 name: null,
                 level: null,
+                hp: null,
                 init: null,
-                characters: this.$store.state.characters
+                characters: this.$store.state.characters,
+                headers: [
+                        { text: 'name', value: 'name' },
+                        { text : 'level', value: 'level'},
+                        { text: 'hp', value: 'hp' },
+                        { text: 'init', value: 'init' },
+                    ]
             }
         },
         methods: {
@@ -37,9 +50,10 @@
                return {
                     name: this.name,
                     level: this.level,
+                    hp: this.hp,
                     init: this.init,
                 }
-            }
+            },
         }
     }
 </script>
