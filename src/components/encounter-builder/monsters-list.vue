024ac5288b654d3a3a8ab88b6648c4d82fab9b05
Dillon Rice <drice89@gmail.com>
2020-12-24 01:48:05 -0800

    reformat select

--- src/components/encounter-builder/monsters-list.vue
+++ src/components/encounter-builder/monsters-list.vue
@@ -1,30 +1,29 @@
 
 <template>
-  <div>
+  <div class="monsters-list-container">
     <v-container>
-      <label for="monsters-pick-list">Monsters</label>
-      <select
-        name="monsters-pick-list"
-        id="monsters-pick-list"
-        class="monster-select"
-        v-model="selectedMonsters"
-        multiple
-      >
-        <option
-          v-for="(monster, index) in monstersByCr"
-          :key="index"
-          :value="monster"
-        >
-          <span>{{ monster.name }}</span>
-          <span>{{ monster.cr }}</span>
-        </option>
-      </select>
+      <div class="monsters-pick-list">
+        Monsters
+        <ul class="monster-select">
+          <li
+            v-for="(monster, index) in monstersByCr"
+            :key="index"
+            :value="monster"
+            @click="addMonsterToRoster(monster)"
+            class="monster-option"
+          >
+            <span>{{ monster.name }}</span>
+            <span>{{ monster.cr }}</span>
+          </li>
+        </ul>
+      </div>
     </v-container>
     <v-container>
       <v-list>
-        <li v-for="(monster, index) in selectedMonsters" :key="index">
+        <li v-for="(monster, index) in selectedMonsters" :key="index" class="">
           <span>{{ monster.xp }}</span>
           <span>{{ monster.name }}</span>
+          <span @click="removeMonsterFromRoster(monster)">X</span>
         </li>
       </v-list>
     </v-container>
@@ -63,52 +62,60 @@ export default {
       return this.getAllMonstersFromState.sort((a, b) => a.cr - b.cr)
     }
   },
-    methods: {
-      generateRandomEncounter(){
-        // get each party member from state
-        // total their level at the selected threshold
-        const totalXp = this.getTotalCharacterLevel(this.threshold)
-        let runningMonXpTotal = 0
-        console.log(totalXp)
-        let finished = false
-        const pool = []
-        while (totalXp && !finished) {
-          let monsters = this.monstersByCr.filter(monster => {
-            const thresholdMultiplier = pool.length > THRESHOLD_MULTIPLIERS_LIMIT ? THRESHOLD_MULTIPLIERS[THRESHOLD_MULTIPLIERS_LIMIT] : THRESHOLD_MULTIPLIERS[pool.length + 1]
-            return (thresholdMultiplier * (monster.xp + runningMonXpTotal)) <= totalXp
-          });
-          if (!monsters.length) {
-            finished = true;
-          } else {
-            const pickedMonster = monsters[(Math.floor(Math.random() * monsters.length))]
-            pool.push(pickedMonster)
-            runningMonXpTotal += pickedMonster.xp
-          }
+  methods: {
+    generateRandomEncounter(){
+      // get each party member from state
+      // total their level at the selected threshold
+      const totalXp = this.getTotalCharacterLevel(this.threshold)
+      let runningMonXpTotal = 0
+      console.log(totalXp)
+      let finished = false
+      const pool = []
+      while (totalXp && !finished) {
+        let monsters = this.monstersByCr.filter(monster => {
+          const thresholdMultiplier = pool.length > THRESHOLD_MULTIPLIERS_LIMIT ? THRESHOLD_MULTIPLIERS[THRESHOLD_MULTIPLIERS_LIMIT] : THRESHOLD_MULTIPLIERS[pool.length + 1]
+          return (thresholdMultiplier * (monster.xp + runningMonXpTotal)) <= totalXp
+        });
+        if (!monsters.length) {
+          finished = true;
+        } else {
+          const pickedMonster = monsters[(Math.floor(Math.random() * monsters.length))]
+          pool.push(pickedMonster)
+          runningMonXpTotal += pickedMonster.xp
         }
-        console.log(runningMonXpTotal)   
-        this.selectedMonsters = pool
       }
-  //       addMonsterToRoster(monster) {
-  //           this.selectedMonsters.push(monster)
-  //       },
-  //       removeMonsterFromRoster(monster) {
-  //           this.selectedMonsters = this.selectedMonsters.filter(m => m.name === monster.name)
-  //       },
-  //       handleMonsterSelectChange(monster) {
-  //           console.log("handlechange")
-  //           if(this.selectedMonsters.some(m => m.name === monster.name)) {
-  //               this.removeMonsterFromRoster(monster);
-  //           } else {
-  //               this.addMonsterToRoster(monster);
-  //           }
-  //       }
-    }
+      console.log(runningMonXpTotal)   
+      this.selectedMonsters = pool
+    },
+    addMonsterToRoster(monster) {
+      let monsterCopy = { id: this.selectedMonsters.length, ...monster }
+      this.selectedMonsters.push(monsterCopy)
+    },
+    removeMonsterFromRoster(monster) {
+      this.selectedMonsters = this.selectedMonsters.filter(m => m.id !== monster.id)
+    },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.monster-select {
-  width: 400px;
-  height: 1000px;
+div.monsters-list-container {
+  display: flex;
+  justify-content: space-around;
+
+  .monster-select {
+    width: 400px;
+    height: 400px;
+    display: block;
+    overflow-y: scroll;
+  }
+  .monster-option {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    .fixed-width {
+      width: 200px;
+    }
+  }
 }
 </style>
